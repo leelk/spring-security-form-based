5b9e7c6ea86a9dc545ae8c87f0928b28b24bfe72
leelk <karunarathne1995@gmail.com>
2023-02-21 17:52:14 +0530

    Initialize

---
+++ src/main/java/me/leel/security/controller/HomeController.java
@@ -0,0 +1,12 @@
+package me.leel.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    public String pring(){
+        return "Hello!!!";
+    }
+}
